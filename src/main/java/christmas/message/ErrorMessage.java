3e3b3df9474b13beadcc51919d214e8372f6f656
hyungjin <hyungzin0309@naver.com>
2023-11-11 09:58:57 +0900

    feat: 메세지

---
+++ src/main/java/christmas/message/ErrorMessage.java
@@ -0,0 +1,12 @@
+package christmas.message;
+
+public class ErrorMessage {
+
+    public static final String INVALID_NUM_INPUT = "[ERROR] 숫자를 입력해 주세요!";
+    public static final String INVALID_DAY_INPUT = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    public static final String INVALID_MENU_INPUT = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+
+    private ErrorMessage() {
+    }
+
+}
---
+++ src/main/java/christmas/message/Message.java
@@ -0,0 +1,20 @@
+package christmas.message;
+
+public class Message {
+
+    public static final String WELCOME = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static final String INPUT_DAY = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String INPUT_MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    public static final String RESULT = "12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    public static final String ORDER_MENU = "<주문 메뉴>";
+    public static final String TOTAL_PRICE_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
+    public static final String GIFT_MENU = "<증정 메뉴>";
+    public static final String BENEFIT_DETAIL = "<혜택 내역>";
+    public static final String TOTAL_BENEFIT = "<총혜택 금액>";
+    public static final String TOTAL_PRICE_AFTER_DISCOUNT = "<할인 후 예상 결제 금액>";
+    public static final String EVENT_BADGE = "<12월 이벤트 배지>";
+
+    private Message() {
+    }
+
+}
